74e870d6607f16210c1aa0c3883578b2947ad8b5
Khushi Singh <khushisingh70951@gmail.com>
2025-07-31 09:59:10 +0530

    Working?

--- src/train.py
+++ src/train.py
@@ -11,7 +11,6 @@ results = model.train(
     imgsz=640,
     scale=0.5,             # S:0.9; M:0.9; L:0.9; X:0.9
     mosaic=1.0,
-    resume=True,  # This tells YOLO to resume from the given weights
     mixup=0.0,             # S:0.05; M:0.15; L:0.15; X:0.2
     copy_paste=0.1,        # S:0.15; M:0.4; L:0.5; X:0.6
     device="0",
